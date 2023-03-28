804c7f0402dbf4517046dd3e4776bbc2531a74d4
claude tsiangana <claudetsiangana@gmail.com>
2023-03-28 20:57:27 +0200

    modifs

--- environment/development.js
+++ environment/development.js
@@ -3,5 +3,4 @@ module.exports = {
     DB_URL: `mongodb+srv://claude_moz:${process.env.DEV_DB_PWD}@cluster0.4mza5.mongodb.net/twitter?retryWrites=true&w=majority`,
     key: path.join(__dirname, '../ssl/locale.key'),
     cert: path.join(__dirname, '../ssl/local.crt'),
-
 }
