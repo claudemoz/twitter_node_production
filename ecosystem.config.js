c640f8dffd8585be54f6dac321544b1ebab1a04f
claude tsiangana <claudetsiangana@gmail.com>
2023-03-29 09:22:00 +0200

    add auto deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,23 +6,22 @@ module.exports = {
     autorestart: true,
     env: {
       NODE_ENV: "development",
-      DEV_DB_PWD: "ClaudeAPI"
     },
     env_production: {
       NODE_ENV: "production",
     }
 }],
 
-  // deploy : {
-  //   production : {
-  //     user : 'SSH_USERNAME',
-  //     host : 'SSH_HOSTMACHINE',
-  //     ref  : 'origin/master',
-  //     repo : 'GIT_REPOSITORY',
-  //     path : 'DESTINATION_PATH',
-  //     'pre-deploy-local': '',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-  //     'pre-setup': ''
-  //   }
-  // }
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '51.38.237.251',
+      ref  : 'origin/main',
+      repo : 'https://github.com/claudemoz/twitter_node_production.git',
+      path : '/home/ubuntu/twitter',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
 };
